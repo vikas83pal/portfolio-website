49c3829c942d7b27e44340472b0c10ba3268bbb8
vikas83pal <vikas83pal@gmail.com>
2026-08-18 10:40:57 +0530

    release

--- components/header.tsx
+++ components/header.tsx
@@ -16,6 +16,7 @@ export default function Header() {
     };
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
+    
   }, []);
 
   return (
