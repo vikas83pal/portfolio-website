0a89a1cec393f466476b0b0b3f738c7568dd0b74
vikas83pal <vikas83pal@gmail.com>
2026-08-30 12:05:24 +0530

    release

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,3 +1,5 @@
+"use client";
+
 import { useActiveSectionContext } from "@/context/active-section-context";
 import { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
